33b025b7d5cdc6fb8376a53e444b3e9d6ea2c711
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-12-30 13:31:53 +0700

    Update main.c

--- Th04A/main.c
+++ Th04A/main.c
@@ -6,4 +6,17 @@ void main() {
     printf("UCLN(12, 8) = %d\n", ucln);
     fflush(stdin);
     getchar();
+    /*
+    4.3 Luyen tap truyen tham so
+    Buoc 6
+    */
+    int a = 0, b = 0;
+    printf("a = ");
+    scanf("%d", &a);
+    printf("b = ");
+    scanf("%d", &b);
+    ucln = UCLN(a, b);
+    printf("UCLN(%d, %d) = %d\n", a, b, ucln);
+    fflush(stdin);
+    getchar();
 }
