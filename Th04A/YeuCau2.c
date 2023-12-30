e2f00e17ed4ffc9d9920f30a2c09f0b9641e2be5
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2023-12-30 15:08:36 +0700

    Create YeuCau2.c

---
+++ Th04A/YeuCau2.c
@@ -0,0 +1,29 @@
+/*
+Input: an integer N.
+Output: all prime numbers which are smaller than N. Then, calculate sum and product of those numbers.
+*/
+#include<stdio.h>
+#include<stdbool.h>
+
+bool isPrime(int);
+
+void main() {
+    int n = 0, sum = 0, product = 1;
+    printf("n = ");
+    scanf("%d", &n);
+    for (int i=2; i<n; i++) {
+        if (isPrime(n)) {
+            printf("i\n");
+            sum += i;
+            product *= i;
+        }
+    }
+    printf("Sum = %d\nProduct = %d\n", sum, product);
+}
+
+bool isPrime(int num) { // Precondition: num is an integer larger than 1.
+    for (int i=2; i<num; i++) {
+        if (num%i == 0) return false;
+    }
+    return true;
+}
