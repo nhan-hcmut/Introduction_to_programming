100749c77dbe5d34cf2958212f2b724bb18fa6d8
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-01-28 23:29:08 +0700

    Update Cau5.c

--- Lab2016/Lab8/Cau5.c
+++ Lab2016/Lab8/Cau5.c
@@ -14,21 +14,21 @@ void main() {
     short a[MAX];
     bool stop = false;
     
-    while (n<1 || n>MAX) {
+    while (n < 1 || n > MAX) {
         printf("Size of the array (from 1 to %d): ", MAX);
         scanf("%hi", &n);
     }
-    for (short i=0; i<n; i++) {
+    for (short i = 0; i < n; i++) {
         do { // Users have to enter a positive integer.
-            printf("Positive number %hi: ", i+1);
+            printf("Positive number %hi: ", i + 1);
             scanf("%hi", &a[i]);
         }
         while (a[i] <= 0);
     }
-    for (short i=2; i>1; i++) { // Check from i=2 that if all integers in array are divisible by i or not.
-        for (short j=0; j<n; j++) {
-            if (a[j]%i != 0) break; // If any integer in array is not divisible by i, we do not need to check others.
-            if (j == n-1) {
+    for (short i = 2; i > 1; i++) { // Check from i=2 that if all integers in array are divisible by i or not.
+        for (short j = 0; j < n; j++) {
+            if (a[j] % i != 0) break; // If any integer in array is not divisible by i, we do not need to check others.
+            if (j == n - 1) {
                 printf("%hi\n", i);
                 stop = true;
             }
