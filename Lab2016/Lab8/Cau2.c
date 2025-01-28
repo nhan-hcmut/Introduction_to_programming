ffa4363e2e4d65cb338a21a6d1fb655f212eef19
nhan-hcmut <147119693+nhan-hcmut@users.noreply.github.com>
2025-01-28 23:24:57 +0700

    Update Cau2.c

--- Lab2016/Lab8/Cau2.c
+++ Lab2016/Lab8/Cau2.c
@@ -14,33 +14,33 @@ void main() {
     short m = 0, size = 0;
     short arr[MAX];
     
-    while (size<1 || size>MAX) {
+    while (size < 1 || size > MAX) {
         printf("Size of array (from 1 to %d): ", MAX);
         scanf("%hi", &size);
     }
-    for (short i=0; i<size; i++) {
+    for (short i = 0; i < size; i++) {
         printf("Number %hi: ", i+1);
         scanf("%hi", &arr[i]);
     }
-    while (m<1 || m>size) {
+    while (m < 1 || m > size) {
         printf("Enter m (from 1 to the size of array): ");
         scanf("%hi", &m);
     }
     bubbleSort(arr, size);
-    for (short i=0; i<m; i++) {
+    
+    for (short i = 0; i < m; i++) {
         if (i == 0) printf("[");
+        
         printf("%hi", arr[i]);
-        if (i < m-1) printf(", ");
+        
+        if (i < m - 1) printf(", ");
         else printf("]\n");
     }
 }
 
 void bubbleSort(short arr[], short size) {
-    
-    for (short i=0; i<size-1; i++) {
-        
-        for (short j=0; j<size-1-i; j++) {
-            
+    for (short i = 0; i < size - 1; i++) {
+        for (short j = 0; j < size - 1 - i; j++) {
             if (arr[j] > arr[j+1]) {
                 short temp = arr[j];
                 arr[j] = arr[j+1];
